3c4d5da05a2560674660613a40a1c541aa2dbea4
Steven <stevendalderop@hotmail.com>
2020-09-07 20:03:04 +0200

    changed permissions

--- application.py
+++ application.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
 
 # engine = chess.engine.SimpleEngine.popen_uci("stockfish-11-win/stockfish-11-win/Windows/stockfish_20011801_x64.exe")
 #engine = chess.engine.SimpleEngine.popen_uci("stockfish-11-linux/stockfish-11-linux/Linux/stockfish_20011801_x64")
-engine = chess.engine.SimpleEngine.popen_uci("stockfish_20090216_x64") # stockfish 12
+engine = chess.engine.SimpleEngine.popen_uci("./stockfish_20090216_x64") # stockfish 12
 
 engine.configure({"UCI_LimitStrength": True, "UCI_Elo": 2850})
 board = chess.Board()

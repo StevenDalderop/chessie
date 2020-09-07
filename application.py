8d1e4e53cb1bc113382828b6983bf70d406e64bd
Steven <stevendalderop@hotmail.com>
2020-09-07 17:42:21 +0200

    dd

--- application.py
+++ application.py
@@ -10,39 +10,16 @@ socketio = SocketIO(app)
 if __name__ == '__main__':
     socketio.run(app)
 
-#engine = chess.engine.SimpleEngine.popen_uci("stockfish-11-win/stockfish-11-win/Windows/stockfish_20011801_x64.exe")
-#engine = chess.engine.SimpleEngine.popen_uci("stockfish-11-linux/stockfish-11-linux/Linux/stockfish_20011801_x64")
+# engine = chess.engine.SimpleEngine.popen_uci("stockfish-11-win/stockfish-11-win/Windows/stockfish_20011801_x64.exe")
+engine = chess.engine.SimpleEngine.popen_uci("stockfish-11-linux/stockfish-11-linux/Linux/stockfish_20011801_x64")
 
-#engine.configure({"UCI_LimitStrength": True, "UCI_Elo": 2850})
+engine.configure({"UCI_LimitStrength": True, "UCI_Elo": 2850})
 board = chess.Board()
-
-async def main() -> None:
-    transport, engine = await chess.engine.popen_uci("stockfish-11-linux/stockfish-11-linux/Linux/stockfish_20011801_x64")
-
-    global board
-    board = chess.Board()
-    while not board.is_game_over():
-        result = await engine.play(board, chess.engine.Limit(time=0.1))
-        board.push(result.move)
-        pc_move = result.move
-        init_board = chess.Board()
-        moves_san = init_board.variation_san(board.move_stack)
-        socketio.emit("announce move pc", {"fen": board.fen(), "move": pc_move.uci(), "moves_san": moves_san})
-
-    await engine.quit()
 
 @app.route("/")
 def index():
     global board
     board = chess.Board() # Reset board
-    fen = board.fen()
-    return render_template("index.html", board = fen)
-
-@app.route("/2")
-def index2():
-    asyncio.set_event_loop_policy(chess.engine.EventLoopPolicy())
-    asyncio.run(main())
-    global board
     fen = board.fen()
     return render_template("index.html", board = fen)
 
@@ -77,8 +54,8 @@ def move(data):
     if (board.is_game_over()):
         emit("announce game over", {"result": board.result()})
 
-    #info = engine.analyse(board, chess.engine.Limit(time=0.5))
-    #emit("announce score", {"score": info["score"].white().score()})
+    info = engine.analyse(board, chess.engine.Limit(time=0.5))
+    emit("announce score", {"score": info["score"].white().score()})
 
 @socketio.on("new game")
 def new_game(data):

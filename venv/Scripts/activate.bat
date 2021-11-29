89969abe82802ab838398086ab2ede6a89e82379
steven <stevendalderop@hotmail.com>
2021-11-29 21:01:59 +0100

    added vs_code

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Steve\Downloads\wetransfer_overzetten_2021-11-28_1708\overzetten\chessie\venv
+set VIRTUAL_ENV=C:\Users\Steve\Documents\Programmeren\chessie\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
